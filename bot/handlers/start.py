ec7246791787c4dbdc1adb244000a932421b6ccb
printnewcode <zenit200803@mail.ru>
2025-03-27 17:59:41 +0300

    pull

--- bot/handlers/start.py
+++ bot/handlers/start.py
@@ -1,14 +1,4 @@
-import telebot
-from telebot import types
-from telebot.types import InlineKeyboardButton, InlineKeyboardMarkup
-from pptx import Presentation
-from pptx.util import Inches
-from django.core.files import File
-from django.core.files.base import ContentFile
 from bot.models import Presentations, User  # Импорт вашей модели в Django
-from io import BytesIO
-import pdfkit  # Для конвертации PPTX в PDF
-import nelsie
 from docx import Document
 from docx.shared import Inches
 import os
@@ -16,9 +6,9 @@ from datetime import datetime
 from django.conf import settings
 
 from bot import bot
-from bot.static.qna import QUESTIONS
-from Realtor.settings import OWNER_ID
-from bot.keyboard import START_BUTTONS, WATER_SUPPLY, AGGREMENT_BUTTON, FLOOR_BUTTONS, TYPE_AREA, TYPE_RENT, SKIP_BUTTON, EXIT_REPLY
+from bot.keyboard import START_BUTTONS, WATER_SUPPLY, AGGREMENT_BUTTON, FLOOR_BUTTONS, TYPE_AREA, TYPE_RENT, \
+    SKIP_BUTTON, EXIT_REPLY
+
 
 def start_message(message):
     bot.clear_step_handler_by_chat_id(message.chat.id)
@@ -29,17 +19,6 @@ def start_message(message):
     bot.send_message(message.chat.id, "Здравствуйте! Я помогу собрать информацию о помещении",
                      reply_markup=START_BUTTONS,
                      )
-
-
-def create_presentation(object):
-    print(object.adress, object.height)
-    slides = nelsie.SlideDeck()
-    for x in range(5):
-        slide = slides.new_slide(bg_color="white")
-        slide.text(object.adress)
-    slides.render("slides.pdf")
-    object.presentation = slides.render("slides.pdf")
-    object.save()
 
 
 def ask_question(call):
@@ -56,12 +35,13 @@ def ask_question(call):
     def create_pres(message):
         if check_exit(message):
             return
-        create_presentation(object=pres)
+        # create_presentation(object=pres)
         bot.send_message(
             text="Информация передана агенту\n\nНаши контакты для связи: +7 933 481 00 01",
             chat_id=message.chat.id,
         )
         return
+
     def agreement(id_):
         msg = bot.send_message(
             text="Для продолжения нужно подтвердить свое согласие на обработку данных, нажав на кнопку ниже",
@@ -69,15 +49,17 @@ def ask_question(call):
             reply_markup=AGGREMENT_BUTTON,
         )
         bot.register_next_step_handler(msg, create_pres)
+
     def register_number(message):
         if check_exit(message):
             return
         pres.contact += message.text
         pres.save()
         return agreement(message.chat.id)
+
     def get_number(id_):
         msg = bot.send_message(
-            text="Телефон для связи", chat_id=id_, 
+            text="Телефон для связи", chat_id=id_,
         )
         bot.register_next_step_handler(msg, register_number)
 
@@ -87,17 +69,19 @@ def ask_question(call):
         pres.contact = f"{message.text}\n"
         pres.save()
         return get_number(message.chat.id)
+
     def get_contact(id_):
         msg = bot.send_message(
-            text="Как к вам обращаться (ФИО)", chat_id=id_, 
+            text="Как к вам обращаться (ФИО)", chat_id=id_,
         )
         bot.register_next_step_handler(msg, register_contact)
+
     def register_additives(message):
         if check_exit(message):
             return
         if message.text == "Пометки отсутствуют":
             return get_contact(message.chat.id)
-    
+
         try:
             pres.additives = message.text
         except Exception as e:
@@ -107,7 +91,8 @@ def ask_question(call):
 
     def get_additives(id_):
         msg = bot.send_message(
-            text="Если у вас остались комментарии и пожелания - напишите их сюда\n\nЕсли пометок нет - нажмите на кнопку", chat_id=id_, reply_markup = SKIP_BUTTON
+            text="Если у вас остались комментарии и пожелания - напишите их сюда\n\nЕсли пометок нет - нажмите на кнопку",
+            chat_id=id_, reply_markup=SKIP_BUTTON
         )
         bot.register_next_step_handler(msg, register_additives)
 
@@ -119,18 +104,18 @@ def ask_question(call):
                 # Получаем информацию о фото (берем последнее, т.к. оно самого высокого качества)
                 file_info = bot.get_file(message.photo[-1].file_id)
                 downloaded_file = bot.download_file(file_info.file_path)
-                
+
                 # Формируем путь для сохранения
                 file_name = f"inside_{message.chat.id}_{message.message_id}.jpg"
                 file_path = os.path.join(settings.MEDIA_ROOT, 'temp', file_name)
-                
+
                 # Создаем директории если их нет
                 os.makedirs(os.path.dirname(file_path), exist_ok=True)
-                
+
                 # Сохраняем файл
                 with open(file_path, 'wb') as new_file:
                     new_file.write(downloaded_file)
-                
+
                 # Сохраняем путь в базу данных
                 pres.photo_inside = file_path
                 pres.save()
@@ -145,7 +130,8 @@ def ask_question(call):
 
     def get_photo_inside(id_):
         msg = bot.send_message(
-            text="Отправьте фото внутри помещения (отправьте как фото, не как файл)", chat_id=id_, reply_markup=EXIT_REPLY
+            text="Отправьте фото внутри помещения (отправьте как фото, не как файл)", chat_id=id_,
+            reply_markup=EXIT_REPLY
         )
         bot.register_next_step_handler(msg, register_photo_inside)
 
@@ -157,18 +143,18 @@ def ask_question(call):
                 # Получаем информацию о фото (берем последнее, т.к. оно самого высокого качества)
                 file_info = bot.get_file(message.photo[-1].file_id)
                 downloaded_file = bot.download_file(file_info.file_path)
-                
+
                 # Формируем путь для сохранения
                 file_name = f"outside_{message.chat.id}_{message.message_id}.jpg"
                 file_path = os.path.join(settings.MEDIA_ROOT, 'temp', file_name)
-                
+
                 # Создаем директории если их нет
                 os.makedirs(os.path.dirname(file_path), exist_ok=True)
-                
+
                 # Сохраняем файл
                 with open(file_path, 'wb') as new_file:
                     new_file.write(downloaded_file)
-                
+
                 # Сохраняем путь в базу данных
                 pres.photo_outside = file_path
                 pres.save()
@@ -183,7 +169,8 @@ def ask_question(call):
 
     def get_photo_outside(id_):
         msg = bot.send_message(
-            text="Отправьте фото снаружи помещения (отправьте как фото, не как файл)", chat_id=id_, reply_markup=EXIT_REPLY
+            text="Отправьте фото снаружи помещения (отправьте как фото, не как файл)", chat_id=id_,
+            reply_markup=EXIT_REPLY
         )
         bot.register_next_step_handler(msg, register_photo_outside)
 
@@ -195,18 +182,18 @@ def ask_question(call):
                 # Получаем информацию о фото (берем последнее, т.к. оно самого высокого качества)
                 file_info = bot.get_file(message.photo[-1].file_id)
                 downloaded_file = bot.download_file(file_info.file_path)
-                
+
                 # Формируем путь для сохранения
                 file_name = f"plan_{message.chat.id}_{message.message_id}.jpg"
                 file_path = os.path.join(settings.MEDIA_ROOT, 'temp', file_name)
-                
+
                 # Создаем директории если их нет
                 os.makedirs(os.path.dirname(file_path), exist_ok=True)
-                
+
                 # Сохраняем файл
                 with open(file_path, 'wb') as new_file:
                     new_file.write(downloaded_file)
-                
+
                 # Сохраняем путь в базу данных
                 pres.plan = file_path
                 pres.save()
@@ -263,6 +250,7 @@ def ask_question(call):
             text="Высота потолков помещения, метры (указывать только цифру)", chat_id=id_, reply_markup=EXIT_REPLY
         )
         bot.register_next_step_handler(msg, register_height)
+
     def register_floor(message):
         if check_exit(message):
             return
@@ -272,10 +260,9 @@ def ask_question(call):
 
     def get_floor(id_):
         msg = bot.send_message(
-            text="Выберите на каком этаже ваше помещение", chat_id=id_, reply_markup=WATER_SUPPLY
+            text="Выберите на каком этаже ваше помещение", chat_id=id_, reply_markup=FLOOR_BUTTONS
         )
         bot.register_next_step_handler(msg, register_floor)
-
 
     def register_water_supply(message):
         if check_exit(message):
@@ -328,6 +315,7 @@ def ask_question(call):
             text="Адрес помещения", chat_id=id_, reply_markup=EXIT_REPLY
         )
         bot.register_next_step_handler(msg, register_adress)
+
     def register_type_building(message):
         if check_exit(message):
             return
@@ -341,10 +329,10 @@ def ask_question(call):
             text="Тип помещения", chat_id=id_, reply_markup=TYPE_AREA
         )
         bot.register_next_step_handler(msg, register_type_building)
-    
 
     get_type_building(call.message.chat.id)
     return
+
 
 def show_all_presentations(call):
     pres = Presentations.objects.all()
@@ -359,7 +347,7 @@ def show_all_presentations(call):
         doc.add_paragraph(f'Высота потолков: {pre.height}')
         doc.add_paragraph(f'Желаемая арендная плата помещения: {pre.rate}')
         doc.add_paragraph(f'Тип аренды помещения: {pre.type_rent}')
-        
+
         # Добавляем план помещения
         if pre.plan:
             try:
@@ -377,7 +365,7 @@ def show_all_presentations(call):
                 print(f"Тип pre.plan: {type(pre.plan)}")
         else:
             print("План отсутствует в записи")
-        
+
         # Добавляем фото снаружи
         if pre.photo_outside:
             try:
@@ -387,7 +375,7 @@ def show_all_presentations(call):
                     doc.add_picture(outside_path, width=Inches(6))
             except Exception as e:
                 print(f"Ошибка при добавлении фото снаружи: {e}")
-        
+
         # Добавляем фото внутри
         if pre.photo_inside:
             try:
@@ -397,7 +385,7 @@ def show_all_presentations(call):
                     doc.add_picture(inside_path, width=Inches(6))
             except Exception as e:
                 print(f"Ошибка при добавлении фото внутри: {e}")
-        
+
         # Добавляем примечания
         if pre.additives and pre.additives != "0":
             doc.add_heading('Дополнительные примечания', level=2)
@@ -406,19 +394,19 @@ def show_all_presentations(call):
         # Сохраняем документ с уникальным именем
         doc_name = f'presentation_{pre.user.user_id}_{datetime.now().strftime("%Y%m%d_%H%M%S")}.docx'
         doc_path = os.path.join(settings.MEDIA_ROOT, 'temp', doc_name)
-        
+
         # Создаем директорию для временных файлов, если её нет
         os.makedirs(os.path.dirname(doc_path), exist_ok=True)
-        
+
         doc.save(doc_path)
-        
+
         # Отправляем документ пользователю
         try:
             with open(doc_path, 'rb') as doc_file:
-                pre.presentation=doc_path
+                pre.presentation = doc_path
                 pre.save()
                 bot.send_document(call.message.chat.id, doc_file, caption="Вот ваша презентация!")
-                
+
         except Exception as e:
             print(f"Ошибка при отправке документа: {e}")
         finally:
@@ -427,6 +415,7 @@ def show_all_presentations(call):
                 os.remove(doc_path)
             except Exception as e:
                 print(f"Ошибка при удалении файла: {e}")
+
 
 @bot.message_handler(commands=['presentations'])
 def handle_presentations(message):
@@ -444,15 +433,25 @@ def handle_presentations(message):
         for pre in pres:
             doc = Document()
 
-            doc.add_heading('Документ', level=1)
-            doc.add_paragraph(f'Здание по адресу: {pre.adress}')
-            doc.add_paragraph(f'Площадь: {pre.square}')
-            doc.add_paragraph(f'Электроснабжение помещения: {pre.power}')
+            doc.add_heading(f'{pre.square} кв.м., {pre.adress}', level=1)
+            doc.add_paragraph(f'Ставка аренды: {pre.rate} руб.')
+            doc.add_paragraph(f'Площадь: {pre.square} кв.м.')
+            if pre.photo_outside:
+                try:
+                    outside_path = os.path.join(settings.MEDIA_ROOT, str(pre.photo_outside))
+                    if os.path.exists(outside_path):
+                        doc.add_heading('Фото снаружи', level=2)
+                        doc.add_picture(outside_path, width=Inches(6))
+                except Exception as e:
+                    print(f"Ошибка при добавлении фото снаружи: {e}")
+            doc.add_heading("Характеристики")
+            doc.add_paragraph(f'Тип помещения: {pre.type_building}')
+            doc.add_paragraph(f'Расположение по адресу: {pre.adress}')
+            doc.add_paragraph(f'Площадь помещения: {pre.square} кв.м.')
+            doc.add_paragraph(f'Мощность: {pre.power} кВт')
             doc.add_paragraph(f'Водоснабжение помещения: {pre.water_supply}')
-            doc.add_paragraph(f'Высота потолков: {pre.height}')
-            doc.add_paragraph(f'Желаемая арендная плата помещения: {pre.rate}')
+            doc.add_paragraph(f'Высота потолков: {pre.height} метра')
             doc.add_paragraph(f'Тип аренды помещения: {pre.type_rent}')
-            
             # Добавляем план помещения
             if pre.plan:
                 try:
@@ -470,18 +469,8 @@ def handle_presentations(message):
                     print(f"Тип pre.plan: {type(pre.plan)}")
             else:
                 print("План отсутствует в записи")
-            
-            # Добавляем фото снаружи
-            if pre.photo_outside:
-                try:
-                    outside_path = os.path.join(settings.MEDIA_ROOT, str(pre.photo_outside))
-                    if os.path.exists(outside_path):
-                        doc.add_heading('Фото снаружи', level=2)
-                        doc.add_picture(outside_path, width=Inches(6))
-                except Exception as e:
-                    print(f"Ошибка при добавлении фото снаружи: {e}")
-            
-            # Добавляем фото внутри
+
+                # Добавляем фото внутри
             if pre.photo_inside:
                 try:
                     inside_path = os.path.join(settings.MEDIA_ROOT, str(pre.photo_inside))
@@ -490,7 +479,7 @@ def handle_presentations(message):
                         doc.add_picture(inside_path, width=Inches(6))
                 except Exception as e:
                     print(f"Ошибка при добавлении фото внутри: {e}")
-            
+
             # Добавляем примечания
             if pre.additives and pre.additives != "0":
                 doc.add_heading('Дополнительные примечания', level=2)
@@ -499,16 +488,17 @@ def handle_presentations(message):
             # Сохраняем документ с уникальным именем
             doc_name = f'presentation_{pre.user.user_id}_{datetime.now().strftime("%Y%m%d_%H%M%S")}.docx'
             doc_path = os.path.join(settings.MEDIA_ROOT, 'temp', doc_name)
-            
+
             # Создаем директорию для временных файлов, если её нет
             os.makedirs(os.path.dirname(doc_path), exist_ok=True)
-            
+
             doc.save(doc_path)
-            
+
             # Отправляем документ пользователю
             try:
                 with open(doc_path, 'rb') as doc_file:
-                    bot.send_document(message.chat.id, doc_file, caption=f"Презентация для помещения по адресу: {pre.adress}")
+                    bot.send_document(message.chat.id, doc_file,
+                                      caption=f"Презентация для помещения по адресу: {pre.adress}")
             except Exception as e:
                 print(f"Ошибка при отправке документа: {e}")
                 bot.reply_to(message, "Произошла ошибка при отправке документа.")
@@ -518,8 +508,7 @@ def handle_presentations(message):
                     os.remove(doc_path)
                 except Exception as e:
                     print(f"Ошибка при удалении файла: {e}")
-                    
+
     except Exception as e:
         print(f"Общая ошибка: {e}")
         bot.reply_to(message, f"Произошла ошибка при создании презентаций: {str(e)}")
-
